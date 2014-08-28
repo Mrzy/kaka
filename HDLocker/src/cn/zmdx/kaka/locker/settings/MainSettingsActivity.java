461d86ddfa95293ff064222c762dbb51c65f7847
zhangyan <zhangyan@hdlocker.com>
2014-08-28 12:59:26 +0800

    change: remove unused note

--- HDLocker/src/cn/zmdx/kaka/locker/settings/MainSettingsActivity.java
+++ HDLocker/src/cn/zmdx/kaka/locker/settings/MainSettingsActivity.java
@@ -8,7 +8,6 @@ public class MainSettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
     }
 }
