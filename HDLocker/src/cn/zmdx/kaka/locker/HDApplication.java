e64cf3b70aaa402b41710ab64a9b9b1589be3f42
zhangyan <zhangyan@hdlocker.com>
2014-08-29 12:25:49 +0800

    change: create HDApplication

---
+++ HDLocker/src/cn/zmdx/kaka/locker/HDApplication.java
@@ -0,0 +1,12 @@
+package cn.zmdx.kaka.locker;
+
+import android.app.Application;
+
+public class HDApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		//TODO
+		super.onCreate();
+	}
+}
