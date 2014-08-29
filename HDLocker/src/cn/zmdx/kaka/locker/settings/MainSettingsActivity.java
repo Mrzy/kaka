2124180bcefaa4d972905f8782bbadeabbecfc6d
shiyanchao <shiyc@hdlocker.com>
2014-08-29 14:42:11 +0800

    feature: add umeng analytics fb and update

--- HDLocker/src/cn/zmdx/kaka/locker/settings/MainSettingsActivity.java
+++ HDLocker/src/cn/zmdx/kaka/locker/settings/MainSettingsActivity.java
@@ -1,6 +1,8 @@
 
 package cn.zmdx.kaka.locker.settings;
 
+import com.umeng.analytics.MobclickAgent;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class MainSettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 }
