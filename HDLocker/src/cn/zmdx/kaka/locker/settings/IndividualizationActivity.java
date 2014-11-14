ec238f7e226c4f7b2503d60e3b0cc9b81effa030
shiyanchao <shiyc@hdlocker.com>
2014-11-14 13:16:32 +0800

    change:修改自定义锁屏背景图片保存路径

--- HDLocker/src/cn/zmdx/kaka/locker/settings/IndividualizationActivity.java
+++ HDLocker/src/cn/zmdx/kaka/locker/settings/IndividualizationActivity.java
@@ -225,6 +225,10 @@ public class IndividualizationActivity extends Activity implements OnClickListen
                 String fileName = PandoraUtils.getRandomString();
                 setBitmap();
                 saveWallpaperFile(fileName);
+                if (isDirect) {
+                    LockScreenManager.getInstance().lock();
+                    onBackPressed();
+                }
                 break;
             case PandoraUtils.REQUEST_CODE_GALLERY: {
                 gotoCropActivity(data.getData());
@@ -275,10 +279,6 @@ public class IndividualizationActivity extends Activity implements OnClickListen
                     PandoraUtils.saveBitmap(PandoraUtils.sLockDefaultThumbBitmap,
                             LOCK_DEFAULT_SDCARD_LOCATION, fileName);
                     saveLockDefaultSP(fileName);
-                    if (isDirect) {
-                        LockScreenManager.getInstance().lock();
-                        onBackPressed();
-                    }
                 }
             }
         }).start();
