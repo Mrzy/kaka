591ce575f894db60dd7d634ba93644863a801a05
zhanglf <zhanglf@hdlocker.com>
2014-12-03 21:26:52 +0800

    change:增加开、关锁屏音效

--- HDLocker/src/cn/zmdx/kaka/locker/sound/LockSoundManager.java
+++ HDLocker/src/cn/zmdx/kaka/locker/sound/LockSoundManager.java
@@ -1,16 +1,21 @@
 
 package cn.zmdx.kaka.locker.sound;
 
+import cn.zmdx.kaka.locker.HDApplication;
+import cn.zmdx.kaka.locker.R;
+import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
 
 public class LockSoundManager {
 
-    public static final int SOUND_ID_LOCK = 1;
+    private static Context mContext = HDApplication.getContext();
 
-    public static final int SOUND_ID_UNLOCK = 2;
+    public static int SOUND_ID_LOCK = 1;
 
-    public static SoundPool sPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+    public static int SOUND_ID_UNLOCK = 2;
+
+    public static SoundPool sPool = null;
 
     static {
         initSoundPool();
@@ -21,6 +26,9 @@ public class LockSoundManager {
      */
     private static void initSoundPool() {
         // TODO
+        sPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        load(R.raw.lock);
+        load(R.raw.unlock);
     }
 
     /**
@@ -30,20 +38,23 @@ public class LockSoundManager {
      */
     public static void play(int soundId) {
         // TODO
+        sPool.play(soundId, 1, 1, 0, 0, 1);
     }
 
     /**
      * 加载音频文件
      */
-    public static void load() {
-        // TODO
+    private static void load(int resId) {
+        SOUND_ID_LOCK = sPool.load(mContext, resId, 1);
     }
 
     /**
      * 释放音频文件
      */
-    public static void unLoad() {
+    private static void unLoad(int soundId) {
         // TODO
+        sPool.unload(soundId);
+
     }
 
     /**
@@ -51,5 +62,8 @@ public class LockSoundManager {
      */
     public static void release() {
         // TODO
+        unLoad(SOUND_ID_LOCK);
+        unLoad(SOUND_ID_UNLOCK);
+        sPool.release();
     }
 }
