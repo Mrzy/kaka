753f597b4abda87fa6846b591556e9ed415e1cb1
zhangyan <zhangyan@hdlocker.com>
2014-12-05 11:25:35 +0800

    change: 更改LockSoundManager的接口

--- HDLocker/src/cn/zmdx/kaka/locker/sound/LockSoundManager.java
+++ HDLocker/src/cn/zmdx/kaka/locker/sound/LockSoundManager.java
@@ -15,55 +15,56 @@ public class LockSoundManager {
 
     public static int SOUND_ID_UNLOCK = 2;
 
-    public static SoundPool sPool = null;
-
-    static {
-        initSoundPool();
-    }
+    private static SoundPool sPool = null;
 
     /**
      * 初始化SoundPool
      */
-    private static void initSoundPool() {
-        // TODO
-        sPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        load(R.raw.lock);
-        load(R.raw.unlock);
+    public static void initSoundPool() {
+        if (sPool != null) {
+            sPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+            load(R.raw.lock);
+            load(R.raw.unlock);
+        }
     }
 
     /**
      * 播放对应音频文件
      * 
-     * @param soundId
+     * @param soundId SOUND_ID_LOCK or SOUND_ID_UNLOCK
      */
     public static void play(int soundId) {
-        // TODO
-        sPool.play(soundId, 1, 1, 0, 0, 1);
+        if (sPool != null) {
+            sPool.play(soundId, 1, 1, 0, 0, 1);
+        }
     }
 
     /**
      * 加载音频文件
      */
     private static void load(int resId) {
-        SOUND_ID_LOCK = sPool.load(mContext, resId, 1);
+        if (sPool != null) {
+            SOUND_ID_LOCK = sPool.load(mContext, resId, 1);
+        }
     }
 
     /**
      * 释放音频文件
      */
     private static void unLoad(int soundId) {
-        // TODO
-        sPool.unload(soundId);
-
+        if (sPool != null) {
+            sPool.unload(soundId);
+        }
     }
 
     /**
      * 释放音频文件
      */
     public static void release() {
-        // TODO
         unLoad(SOUND_ID_LOCK);
         unLoad(SOUND_ID_UNLOCK);
-        sPool.release();
+        if (sPool != null) {
+            sPool.release();
+        }
     }
 }
