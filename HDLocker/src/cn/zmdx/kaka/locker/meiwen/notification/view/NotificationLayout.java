c103ef50ec03c6a5c70283b5fd66a28010388ae2
shiyanchao <shiyc@hdlocker.com>
2015-01-15 13:25:37 +0800

    change修复导致崩溃的bug

--- HDLocker/src/cn/zmdx/kaka/locker/meiwen/notification/view/NotificationLayout.java
+++ HDLocker/src/cn/zmdx/kaka/locker/meiwen/notification/view/NotificationLayout.java
@@ -66,8 +66,8 @@ public class NotificationLayout extends LinearLayout {
 
     public NotificationLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init();
         mContext = context;
+        init();
     }
 
     public NotificationLayout(Context context, AttributeSet attrs) {
