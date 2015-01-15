ba25d7c6586a231b97e7d2dbd88177b7fcc18ae0
shiyanchao <shiyc@hdlocker.com>
2015-01-15 11:43:18 +0800

    bug:解决微信正式包分享不成功的bug：增加屏蔽微信分享jar包的混淆代码

--- HDLocker/src/cn/zmdx/kaka/locker/wxapi/WXEntryActivity.java
+++ HDLocker/src/cn/zmdx/kaka/locker/wxapi/WXEntryActivity.java
@@ -1,9 +1,9 @@
+
 package cn.zmdx.kaka.locker.wxapi;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.tencent.mm.sdk.openapi.BaseReq;
 import com.tencent.mm.sdk.openapi.BaseResp;
@@ -16,77 +16,75 @@ import com.umeng.analytics.MobclickAgent;
 
 public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
 
-	private IWXAPI api;
-	private static String APPID = "wx5fa094ca2b1994ba";
+    private IWXAPI api;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		api = WXAPIFactory.createWXAPI(this, APPID, true);
-		api.handleIntent(getIntent(), this);
-	}
+    private static String APPID = "wx5fa094ca2b1994ba";
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		MobclickAgent.onResume(this);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        api = WXAPIFactory.createWXAPI(this, APPID, true);
+        api.handleIntent(getIntent(), this);
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		MobclickAgent.onPause(this);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
 
-	@Override
-	protected void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
 
-		setIntent(intent);
-		api.handleIntent(intent, this);
-	}
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
 
-	@Override
-	public void onReq(BaseReq req) {
-		switch (req.getType()) {
-		case ConstantsAPI.COMMAND_GETMESSAGE_FROM_WX:
-			goToSplash();
-			// 在微信中点击豆丁书房
-			break;
-		case ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX:
-			goToBookReader((ShowMessageFromWX.Req) req);
-			// 发送方,接收方点击
-			break;
-		default:
-			break;
-		}
-	}
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
 
-	@Override
-	public void onResp(BaseResp resp) {
+    @Override
+    public void onReq(BaseReq req) {
+        switch (req.getType()) {
+            case ConstantsAPI.COMMAND_GETMESSAGE_FROM_WX:
+                goToSplash();
+                // 在微信中点击豆丁书房
+                break;
+            case ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX:
+                goToBookReader((ShowMessageFromWX.Req) req);
+                // 发送方,接收方点击
+                break;
+            default:
+                break;
+        }
+    }
 
-		switch (resp.errCode) {
-		case BaseResp.ErrCode.ERR_OK:
-		    Log.d("syc", "发送成功！");
-			finish();
-			break;
-		case BaseResp.ErrCode.ERR_USER_CANCEL:
-		    Log.d("syc", "发送shibai！");
-			finish();
-			break;
-		case BaseResp.ErrCode.ERR_AUTH_DENIED:
-		    Log.d("syc", "发送beijijue！");
-			finish();
-			break;
-		}
+    @Override
+    public void onResp(BaseResp resp) {
 
-	}
+        switch (resp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                finish();
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:
+                finish();
+                break;
+            case BaseResp.ErrCode.ERR_AUTH_DENIED:
+                finish();
+                break;
+        }
 
-	private void goToSplash() {
-		finish();
-	}
+    }
 
-	private void goToBookReader(ShowMessageFromWX.Req showReq) {
-		finish();
-	}
+    private void goToSplash() {
+        finish();
+    }
+
+    private void goToBookReader(ShowMessageFromWX.Req showReq) {
+        finish();
+    }
 }
