2bbec06f1c029d690a52292213742d8759444b67
zhangyan <zhangyan@hdlocker.com>
2015-02-10 21:21:25 +0800

    change: 取消通知引导

--- HDLocker/src/cn/zmdx/kaka/fast/locker/notification/guide/NotificationGuideHelper.java
+++ HDLocker/src/cn/zmdx/kaka/fast/locker/notification/guide/NotificationGuideHelper.java
@@ -11,29 +11,31 @@ import cn.zmdx.kaka.fast.locker.settings.config.PandoraConfig;
 public class NotificationGuideHelper {
 
     public static NotificationInfo getNextGuide(Context context) {
-        if (!checkDeviceNotificationServiceAvailable(context)) {
-            return null;
-        }
-
-        //如果是第一次打开锁屏，会提示解锁及下拉的引导，避免过多引导，这里先不显示通知引导
-        if (PandoraConfig.newInstance(context).getLockScreenTimes() < 1) {
-            return null;
-        }
-
-        int preProgress = getGuideProgress(context);
-        switch (preProgress) {
-            case 0:// 通知功能说明，提示双击移除
-                return PandoraNotificationFactory.createGuideRemoveNotification();
-//            case 1:// 教学打开通知详细
-//                return PandoraNotificationFactory.createGuideOpenNotificationDetail();
-            case 1:// 教学开启通知教程
-                if (PandoraNotificationService.sNotificationServiceRunning) {
-                    return null;
-                }
-                return PandoraNotificationFactory.createGuideOpenNotifyPermissionNotification();
-            default:
-        }
+        // 暂时取消通知引导
         return null;
+//        if (!checkDeviceNotificationServiceAvailable(context)) {
+//            return null;
+//        }
+//
+//        //如果是第一次打开锁屏，会提示解锁及下拉的引导，避免过多引导，这里先不显示通知引导
+//        if (PandoraConfig.newInstance(context).getLockScreenTimes() < 1) {
+//            return null;
+//        }
+//
+//        int preProgress = getGuideProgress(context);
+//        switch (preProgress) {
+//            case 0:// 通知功能说明，提示双击移除
+//                return PandoraNotificationFactory.createGuideRemoveNotification();
+////            case 1:// 教学打开通知详细
+////                return PandoraNotificationFactory.createGuideOpenNotificationDetail();
+//            case 1:// 教学开启通知教程
+//                if (PandoraNotificationService.sNotificationServiceRunning) {
+//                    return null;
+//                }
+//                return PandoraNotificationFactory.createGuideOpenNotifyPermissionNotification();
+//            default:
+//        }
+//        return null;
     }
 
     public static boolean checkDeviceNotificationServiceAvailable(Context context) {
