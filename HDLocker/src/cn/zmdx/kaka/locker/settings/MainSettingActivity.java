54ead49943b1f8124dde86b39c6da253b9477cd7
shiyanchao <shiyc@hdlocker.com>
2015-04-30 16:37:15 +0800

    change:增加应用宝首发元素；去掉无用资源

--- HDLocker/src/cn/zmdx/kaka/locker/settings/MainSettingActivity.java
+++ HDLocker/src/cn/zmdx/kaka/locker/settings/MainSettingActivity.java
@@ -106,15 +106,15 @@ public class MainSettingActivity extends ActionBarActivity implements IMainSetti
     }
 
     public void handleToActivity() {
-        String appMetaData = PandoraUtils.getAppMetaData(this, "UMENG_CHANNEL");
-        if ("sougou".equals(appMetaData)) {
-            gotoSplash();
-        } else {
+//        String appMetaData = PandoraUtils.getAppMetaData(this, "UMENG_CHANNEL");
+//        if ("sougou".equals(appMetaData)) {
+//            gotoSplash();
+//        } else {
             isFirstIn = !PandoraConfig.newInstance(this).isHasGuided();
             if (isFirstIn) {
                 goInitSetting();
             }
-        }
+//        }
 
     }
 
